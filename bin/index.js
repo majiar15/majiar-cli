4d59842b1f4888de884005ec7c3db371775c16b3
Martin Jimenez <majiar16@gmail.com>
2021-11-19 17:57:05 -0500

    add majiar name

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const yargs_1 = __importDefault(require("yargs"));
 const helpers_1 = require("yargs/helpers");
+const figlet_1 = __importDefault(require("figlet"));
 const utils_1 = require("./utils");
 const usage = "\nAyuda: Majiar-cli ofrece las siguientes funciones";
 const options = yargs_1.default
@@ -13,21 +14,35 @@ const options = yargs_1.default
     .option("gc", { alias: "generateController", describe: "crea un controlador nuevo", type: "string", demandOption: false })
     .option("gr", { alias: "generateRoute", describe: "crea un router nuevo", type: "string", demandOption: false })
     .option("gm", { alias: "generateModel", describe: "crea un modelo nuevo", type: "string", demandOption: false })
-    .help(true)
-    .argv;
+    .help(true);
 const argv = (0, yargs_1.default)((0, helpers_1.hideBin)(process.argv)).argv;
 //@ts-ignore            
 if (yargs_1.default.argv.gc || yargs_1.default.argv.generateController) {
     // @ts-ignore
     (0, utils_1.createController)(yargs_1.default.argv.gc || yargs_1.default.argv.generateController, false);
+    //@ts-ignore            
 }
-//@ts-ignore            
-if (yargs_1.default.argv.gr || yargs_1.default.argv.generateRoute) {
+else if (yargs_1.default.argv.gr || yargs_1.default.argv.generateRoute) {
     //@ts-ignore            
     (0, utils_1.createRoute)(yargs_1.default.argv.gr || yargs_1.default.argv.generateRoute, false);
+    //@ts-ignore
 }
-//@ts-ignore
-if (yargs_1.default.argv.gm || yargs_1.default.argv.generateModel) {
+else if (yargs_1.default.argv.gm || yargs_1.default.argv.generateModel) {
     //@ts-ignore            
     (0, utils_1.createModel)(yargs_1.default.argv.gm || yargs_1.default.argv.generateModel, false);
+}
+else {
+    figlet_1.default.text('Majiar', {
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 100,
+        whitespaceBreak: true
+    }, function (err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+    });
 }
--- index.ts
+++ index.ts
@@ -1,36 +1,45 @@
 #! /usr/bin/env node
 import yargs from 'yargs';
 import {hideBin}from 'yargs/helpers'; 
+import figlet from 'figlet'; 
 import {createController, createRoute, createModel} from './utils';
+
 const usage = "\nAyuda: Majiar-cli ofrece las siguientes funciones";const options = yargs  
 .usage(usage)
-      .option("gc", {alias:"generateController", describe: "crea un controlador nuevo", type: "string", demandOption
+.option("gc", {alias:"generateController", describe: "crea un controlador nuevo", type: "string", demandOption
 : false })
 .option("gr", {alias:"generateRoute", describe: "crea un router nuevo", type: "string", demandOption
 : false })
 .option("gm", {alias:"generateModel", describe: "crea un modelo nuevo", type: "string", demandOption
 : false })                                                                                                    
-.help(true)  
-      .argv;
-      
+.help(true); 
 const argv = yargs(hideBin(process.argv)).argv
-
 //@ts-ignore            
 if(yargs.argv.gc || yargs.argv.generateController ){
     // @ts-ignore
 createController(yargs.argv.gc || yargs.argv.generateController,false);
-}
 //@ts-ignore            
-if(yargs.argv.gr || yargs.argv.generateRoute ){
+}else if(yargs.argv.gr || yargs.argv.generateRoute ){
 //@ts-ignore            
 createRoute(yargs.argv.gr || yargs.argv.generateRoute,false);
 
-    
-}
 //@ts-ignore
- if(yargs.argv.gm || yargs.argv.generateModel  ){
+}else if(yargs.argv.gm || yargs.argv.generateModel  ){
 //@ts-ignore            
 createModel(yargs.argv.gm || yargs.argv.generateModel,false);
-
-
+}else{
+      figlet.text('Majiar', {
+      
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 100,
+            whitespaceBreak: true
+        }, function(err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            console.log(data);
+        });     
 }
